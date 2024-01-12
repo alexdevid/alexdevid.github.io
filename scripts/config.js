bfce8ede1859034ae44d215bbf5b8a4baf9701a3
Alexander Tsukanov <kerdevid@gmail.com>
2024-01-12 15:14:44 +0100

    initial v2

--- scripts/config.js
+++
@@ -1,9 +0,0 @@
-var Config = {
-    boxWidth: window.innerWidth,
-    boxHeight: window.innerHeight,
-    ballsCount: 10,
-    ballsRadius: 30
-};
-var Models = {};
-var Core = {};
-var Globals = {};
